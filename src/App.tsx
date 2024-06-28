8a55b455334017155c557395d494df29e17fe362
AlexSerdyuk83 <a.galko83@gmail.com>
2024-06-28 10:39:12 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import './App.css';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const app = (window as any).Telegram?.WebApp;
+
+function App() {
+
+  useEffect(() => {
+    if (app) {
+      app.ready();
+    }
+  }, []);
+
+  return <div>Epic Connect</div>;
+}
+
+export default App;
---
+++ telegram.d.ts
@@ -0,0 +1,42 @@
+interface TelegramWebApp {
+  version: string;
+  platform: string;
+  themeParams: {
+    [key: string]: string;
+  };
+  colorScheme: 'light' | 'dark';
+  isExpanded: boolean;
+  viewportHeight: number;
+  viewportStableHeight: number;
+  headerColor: string;
+  backgroundColor: string;
+  BackButton: {
+    isVisible: boolean;
+    show: () => void;
+    hide: () => void;
+    onClick: (callback: () => void) => void;
+  };
+  MainButton: {
+    isVisible: boolean;
+    setText: (text: string) => void;
+    show: () => void;
+    hide: () => void;
+    enable: () => void;
+    disable: () => void;
+    onClick: (callback: () => void) => void;
+  };
+  HapticFeedback: {
+    impactOccurred: (style: 'light' | 'medium' | 'heavy') => void;
+    notificationOccurred: (type: 'error' | 'success' | 'warning') => void;
+    selectionChanged: () => void;
+  };
+  ready: () => void;
+  expand: () => void;
+  close: () => void;
+}
+
+interface Window {
+  Telegram: {
+    WebApp: TelegramWebApp;
+  };
+}
